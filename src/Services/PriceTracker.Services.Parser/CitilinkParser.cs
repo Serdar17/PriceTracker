075c96959cf1ba8200bd76663f1da0da4c3117c1
Serdar Ablyametov <96997312+Serdar17@users.noreply.github.com>
2024-01-29 17:12:02 +0500

    feat: change citilink parser

--- src/Services/PriceTracker.Services.Parser/CitilinkParser.cs
+++ src/Services/PriceTracker.Services.Parser/CitilinkParser.cs
@@ -12,26 +12,25 @@ namespace PriceTracker.Services.Parser
             IWebDriver driver = DriverConfig.GetConfiguredWebDriver();
             var title = string.Empty;
             double price = 0;
-            double price_card = 0;
+            double priceCard = 0;
             try
             {
                 driver.Navigate().GoToUrl(url);
                 title = driver.FindElement(By.XPath(".//h1[@class='e1ubbx7u0 eml1k9j0 app-catalog-tn2wxd e1gjr6xo0']"))
                     .Text;
-                var find_price = driver.FindElement(By.XPath(".//div[@data-meta-name='PriceBlock__price']/span"));
-                price = double.Parse(Regex.Replace(find_price.Text, @"[^\d]", ""));
-                var find_card_price =
+                var findPrice = driver.FindElement(By.XPath(".//div[@data-meta-name='PriceBlock__price']/span"));
+                price = double.Parse(Regex.Replace(findPrice.Text, @"[^\d]", ""));
+                var findCardPrice =
                     driver.FindElement(By.XPath(".//div[@data-meta-name='PriceBlock__club-price']/span[1]/span[1]"));
-                price_card = double.Parse(Regex.Replace(find_card_price.Text, @"[^\d]", ""));
+                priceCard = double.Parse(Regex.Replace(findCardPrice.Text, @"[^\d]", ""));
             }
             catch (NoSuchElementException e)
             {
-                var find_price = driver.FindElement(By.XPath(".//div[@data-meta-name='PriceBlock__price']/span"));
-                price = double.Parse(Regex.Replace(find_price.Text, @"[^\d]", ""));
+                var findPrice = driver.FindElement(By.XPath(".//div[@data-meta-name='PriceBlock__price']/span"));
+                price = double.Parse(Regex.Replace(findPrice.Text, @"[^\d]", ""));
             }
             catch (Exception ex)
             {
-                price = -1;
                 return null;
             }
             finally
@@ -39,7 +38,7 @@ namespace PriceTracker.Services.Parser
                 driver.Close();
             }
 
-            return new ParseResult(title, price, price_card);
+            return new ParseResult(title, price, priceCard);
         }
     }
 }
